2a73b40276439f36775017977e31525e8b39ccc6
Evans <ellaboevans@gmail.com>
2023-05-11 17:37:36 +0000

    First Commit

---
+++ final-project/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className=" flex flex-col justify-center items-center h-screen">
+      <p className="text-center text-3xl font-bold text-slate-800">
+        This is frontend Mentor Challenge
+      </p>
+      
+      <p className="font-semibold mt-8 text-2xl">Coming Soon!!!</p> 
+    </div>
+  );
+};
+
+export default App;
