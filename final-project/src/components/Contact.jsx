ddf36b386c72c92cc27cc03bef70402e72caabab
Evans <ellaboevans@gmail.com>
2023-05-12 02:13:13 +0000

    Performing Email Validations

--- final-project/src/components/Contact.jsx
+++ final-project/src/components/Contact.jsx
@@ -1,4 +1,33 @@
+import React, { useEffect } from "react";
+
 const Contact = () => {
+  const [email, setEmail] = React.useState("");
+  const [alertMessage, setAlertMessage] = React.useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAlertMessage(false);
+    }, 3000);
+    timer;
+    return () => clearTimeout(timer);
+  });
+
+  // Email Validation
+  const isEmailValid = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!isEmailValid(email)) {
+      setAlertMessage(true);
+    } else {
+      setAlertMessage(false);
+      console.log({ Email: email });
+    }
+
+    setEmail("");
+  };
+
   return (
     <section className=" flex flex-col items-center justify-center -mt-40 bg-email-bg text-neutral-white  rounded shadow-md absolute mx-4 right-0 top-auto">
       <div className="text-center p-2">
@@ -10,20 +39,33 @@ const Contact = () => {
         </p>
       </div>
       <div className=" mb-6">
-        <form action="#" method="post" className="flex items-center flex-col">
+        <form
+          onSubmit={handleSubmit}
+          method="post"
+          className="flex items-center flex-col"
+        >
           <input
-            type="email"
+            type="text"
             placeholder="email@example.com"
             className="p-3 px-6 rounded-full mt-4 mb-6 placeholder:text-sm focus:outline-none focus-visible:text-slate-800 font-semibold"
-            required
+            value={email.trim()}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <button
             type="submit"
             className="p-3 px-6 w-full rounded-full font-semibold bg-inside-cta-blue focus:outline-none"
+            disabled={!email}
           >
             Get Started For Free
           </button>
         </form>
+        {alertMessage && (
+          <div>
+            <p className="text-error-alert text-center pt-4">
+              Please enter a valid email address
+            </p>
+          </div>
+        )}
       </div>
     </section>
   );
